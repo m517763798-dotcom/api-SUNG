f6509f6ace4545f0ea3dc7e22a7e781b638db0da
m517763798-dotcom <m517763798@gmail.com>
2026-01-06 13:48:01 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({
+module.exports = (req, res) => {
+  res.json({
     name: "TikTok Downloader - SUNG-K4",
     version: "2.0.0",
     usage: "GET /api/tiktok?url={TikTok_video_link}",
     developer: "SUNG-K4"
   });
-}
+};
